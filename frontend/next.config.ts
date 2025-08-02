7f16bd883c70d93c68982e08695e5d8cfaf648e7
Aahil Shaikh <aahil.shaikh@twelvelabs.io>
2025-08-02 08:06:37 +0000

    SAFE: add API proxy something

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,6 +17,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://143.198.61.27:8000/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
